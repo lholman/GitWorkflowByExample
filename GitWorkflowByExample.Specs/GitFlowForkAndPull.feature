e30157c8e8271e9bdb275d3cca99a2c5e77f8f2c
lholman <lloyd.holman@datumgenerics.net>
2014-04-03 00:36:37 +0100

    Initial cut of GitWorkflowByExample
    Contains an example feature and scenarios, requires integration with
    github API to proceed

---
+++ GitWorkflowByExample.Specs/GitFlowForkAndPull.feature
@@ -0,0 +1,21 @@
+﻿Feature: GitFlow ForkAndPull
+	In order to make changes to a software product
+	As a technical resource
+	I want my code changes integrated in to the software product
+
+	Scenario: Stage changes locally
+		Given I have an upstream repository "lloydstone/femah"
+			And I have a fork of the upstream repository "lholman/femah"
+			And I have a local clone of the fork
+			And I have a new local branch "how-to-gitflow-fork-and-pull"
+		When I make a code change and commit
+		Then the local branch is ahead of the fork "lholman/femah" by one commit
+
+	Scenario: Push changes to fork
+		Given I have an upstream repository "lloydstone/femah"
+			And I have a fork of the upstream repository "lholman/femah"
+			And I have a local clone of the fork
+			And I have a local branch "how-to-gitflow-fork-and-pull" one commit ahead of the fork "lholman/femah"
+		When I push the commit to the fork "lholman/femah"
+		Then the fork "lholman/femah" is in sync with the local branch "how-to-gitflow-fork-and-pull"
+			And the fork "lholman/femah" is one commit ahead of the upstream repository "lloydstone/femah"
---
+++ GitWorkflowByExample.Specs/GitFlowForkAndPullSteps.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using Octokit;
+using TechTalk.SpecFlow;
+
+namespace GitWorkflowByExample.Specs
+{
+    [Binding]
+    public class GitFlowForkAndPullSteps
+    {
+        [Given(@"I have an upstream repository ""(.*)""")]
+        public void GivenIHaveAnUpstreamRepository(string p0)
+        {
+            //ScenarioContext.Current.Pending();
+            //var github = new GitHubClient(new ProductHeaderValue("MyAmazingApp"));
+            var repoClient = new RepositoriesClient(new ApiConnection(new Connection(new ProductHeaderValue("femah"))));
+            //var user = await github.User.Get("lholman");
+            var repo = repoClient.Get("lholman", "Lloyd Holman");
+            //Console.WriteLine(user.Followers + " folks love the half ogre!");
+        }
+        
+        [Given(@"I have a fork of the upstream repository ""(.*)""")]
+        public void GivenIHaveAForkOfTheUpstreamRepository(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"I have a local clone of the fork")]
+        public void GivenIHaveALocalCloneOfTheFork()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"I have a new local branch ""(.*)""")]
+        public void GivenIHaveANewLocalBranch(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"I make a code change and commit")]
+        public void WhenIMakeACodeChangeAndCommit()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"the local branch is ahead of the fork ""(.*)"" by one commit")]
+        public void ThenTheLocalBranchIsAheadOfTheForkByOneCommit(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Given(@"I have a local branch ""(.*)"" one commit ahead of the fork ""(.*)""")]
+        public void GivenIHaveALocalBranchOneCommitAheadOfTheFork(string p0, string p1)
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [When(@"I push the commit to the fork ""(.*)""")]
+        public void WhenIPushTheCommitToTheFork(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"the fork ""(.*)"" is in sync with the local branch ""(.*)""")]
+        public void ThenTheForkIsInSyncWithTheLocalBranch(string p0, string p1)
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"the fork ""(.*)"" is one commit ahead of the upstream repository ""(.*)""")]
+        public void ThenTheForkIsOneCommitAheadOfTheUpstreamRepository(string p0, string p1)
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+    }
+}
